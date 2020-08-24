dcff024be4f0ffa4b6fc667d9e566ac36cebc03a
Marina <marinafaenze@hotmail.com>
2020-08-24 16:55:36 -0300

    table with fixex header created

--- src/main/webapp/app/entities/sales/sales.tsx
+++ src/main/webapp/app/entities/sales/sales.tsx
@@ -10,90 +10,99 @@ import { getEntities } from './sales.reducer';
 import { ISales } from 'app/shared/model/sales.model';
 import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
 
+
+import Paper from '@material-ui/core/Paper';
+import Table1 from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import { makeStyles } from '@material-ui/core/styles';
+
 export interface ISalesProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: 440,
+  },
+});
+
 export const Sales = (props: ISalesProps) => {
+  const classes = useStyles();
+
   useEffect(() => {
     props.getEntities();
   }, []);
 
   const { salesList, match, loading } = props;
   return (
+    
     <div>
       <h2 id="sales-heading">
         <Translate contentKey="testApp.sales.home.title">Sales</Translate>
-        <Link to={`${match.url}/new`} className="btn btn-primary float-right jh-create-entity" id="jh-create-entity">
-          <FontAwesomeIcon icon="plus" />
-          &nbsp;
-          <Translate contentKey="testApp.sales.home.createLabel">Create new Sales</Translate>
+        <Link to={`${match.url}/new`} className="float-right">
+          <Fab color="primary" aria-label="add"><AddIcon /></Fab>
         </Link>
       </h2>
+
       <div className="table-responsive">
-        {salesList && salesList.length > 0 ? (
-          <Table responsive>
-            <thead>
-              <tr>
-                <th>
-                  <Translate contentKey="global.field.id">ID</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="testApp.sales.description">Description</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="testApp.sales.state">State</Translate>
-                </th>
-                <th>
-                  <Translate contentKey="testApp.sales.date">Date</Translate>
-                </th>
-                <th />
-              </tr>
-            </thead>
-            <tbody>
-              {salesList.map((sales, i) => (
-                <tr key={`entity-${i}`}>
-                  <td>
-                    <Button tag={Link} to={`${match.url}/${sales.id}`} color="link" size="sm">
-                      {sales.id}
-                    </Button>
-                  </td>
-                  <td>{sales.description}</td>
-                  <td>
-                    <Translate contentKey={`testApp.State.${sales.state}`} />
-                  </td>
-                  <td>{sales.date ? <TextFormat type="date" value={sales.date} format={APP_LOCAL_DATE_FORMAT} /> : null}</td>
-                  <td className="text-right">
-                    <div className="btn-group flex-btn-group-container">
-                      <Button tag={Link} to={`${match.url}/${sales.id}`} color="info" size="sm">
-                        <FontAwesomeIcon icon="eye" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.view">View</Translate>
-                        </span>
-                      </Button>
-                      <Button tag={Link} to={`${match.url}/${sales.id}/edit`} color="primary" size="sm">
-                        <FontAwesomeIcon icon="pencil-alt" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.edit">Edit</Translate>
-                        </span>
-                      </Button>
-                      <Button tag={Link} to={`${match.url}/${sales.id}/delete`} color="danger" size="sm">
-                        <FontAwesomeIcon icon="trash" />{' '}
-                        <span className="d-none d-md-inline">
-                          <Translate contentKey="entity.action.delete">Delete</Translate>
-                        </span>
-                      </Button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        ) : (
-          !loading && (
-            <div className="alert alert-warning">
-              <Translate contentKey="testApp.sales.home.notFound">No Sales found</Translate>
-            </div>
-          )
-        )}
+      {
+        salesList && salesList.length > 0 
+        ? (
+          <Paper className={classes.root}>
+            <TableContainer className={classes.container}>
+              <Table1 stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>             
+                      <TableCell> ID </TableCell>
+                      <TableCell> Description </TableCell>
+                      <TableCell> State </TableCell>
+                      <TableCell> Date </TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {salesList.map((sales, i) => (
+                    <TableRow key={`entity-${i}`} hover role="checkbox" tabIndex={-1}>
+                      <TableCell>
+                        <Button tag={Link} to={`${match.url}/${sales.id}`} color="link" size="sm">
+                          {sales.id}
+                        </Button>
+                      </TableCell>
+
+                      <TableCell>
+                        {sales.description}
+                      </TableCell>
+
+                      <TableCell>
+                        <Translate contentKey={`testApp.State.${sales.state}`} />
+                      </TableCell>
+
+                      <TableCell>
+                        {sales.date ? <TextFormat type="date" value={sales.date} format={APP_LOCAL_DATE_FORMAT} /> : null}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table1>        
+            </TableContainer>
+          </Paper>
+        ) 
+        : ( !loading && (
+              <div className="alert alert-warning">
+                <Translate contentKey="testApp.sales.home.notFound">No Sales found</Translate>
+              </div>
+            )
+        )
+      }
       </div>
     </div>
   );
