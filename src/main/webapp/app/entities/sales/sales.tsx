4efa260dc9ad1ea8c97c947bb4380cc0e5a14492
Marina <marinafaenze@hotmail.com>
2020-08-24 18:41:33 -0300

    style added to sales

--- src/main/webapp/app/entities/sales/sales.tsx
+++ src/main/webapp/app/entities/sales/sales.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
-import { Button, Col, Row, Table } from 'reactstrap';
+import { Col, Row, Table } from 'reactstrap';
 import { Translate, ICrudGetAllAction, TextFormat } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
@@ -23,10 +23,13 @@ import TableRow from '@material-ui/core/TableRow';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import FindInPageIcon from '@material-ui/icons/FindInPage';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 export interface ISalesProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
@@ -35,8 +38,14 @@ const useStyles = makeStyles({
     width: '100%',
   },
   container: {
-    maxHeight: 440,
+    maxHeight: 600,
   },
+  tableHead: {
+    backgroundColor: '#000000'
+  },
+  link: {
+    color: '#004f87'
+  }
 });
 
 export const Sales = (props: ISalesProps) => {
@@ -50,12 +59,14 @@ export const Sales = (props: ISalesProps) => {
   return (
     
     <div>
-      <h2 id="sales-heading">
-        <Translate contentKey="testApp.sales.home.title">Sales</Translate>
-        <Link to={`${match.url}/new`} className="float-right">
-          <Fab color="primary" aria-label="add"><AddIcon /></Fab>
-        </Link>
-      </h2>
+      <Typography variant="h3" align="center" component="h1" className={classes.link} gutterBottom>
+        <Box fontWeight="fontWeightBold" letterSpacing={2}>
+          Ventas
+          <Link to={`${match.url}/new`} className="float-right">
+            <Fab size="small" color="primary" aria-label="add"><AddIcon /></Fab>
+          </Link>
+        </Box>
+      </Typography>
 
       <div className="table-responsive">
       {
@@ -65,12 +76,32 @@ export const Sales = (props: ISalesProps) => {
             <TableContainer className={classes.container}>
               <Table1 stickyHeader aria-label="sticky table">
                 <TableHead>
-                  <TableRow>
-                    <TableCell> Actions </TableCell>                  
-                    <TableCell> Id </TableCell>
-                    <TableCell> Description </TableCell>
-                    <TableCell> State </TableCell>
-                    <TableCell> Date </TableCell>
+                  <TableRow className={classes.tableHead}>
+                    <TableCell align="center">
+                      <Typography>
+                        <Box fontWeight="fontWeightBold" fontSize={18}>Actions</Box>
+                      </Typography>
+                    </TableCell>                  
+                    <TableCell align="center">
+                      <Typography>
+                        <Box fontWeight="fontWeightBold" fontSize={18}>Id</Box>
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Typography>
+                        <Box fontWeight="fontWeightBold" fontSize={18}>Description</Box>
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Typography>
+                        <Box fontWeight="fontWeightBold" fontSize={18}>State</Box>
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="center">
+                      <Typography>
+                        <Box fontWeight="fontWeightBold" fontSize={18}>Date</Box>
+                      </Typography>
+                    </TableCell>
                   </TableRow>
                 </TableHead>
 
@@ -78,27 +109,41 @@ export const Sales = (props: ISalesProps) => {
                   {salesList.map((sales, i) => (                    
                     <TableRow key={`entity-${i}`} hover role="checkbox" tabIndex={-1}>
 
-                      <TableCell>
-                        <IconButton aria-label="findinpage"><FindInPageIcon fontSize="small"/></IconButton>
-                        <IconButton aria-label="delete"><EditIcon fontSize="small" /></IconButton>
-                        <IconButton aria-label="delete"><DeleteIcon fontSize="small" /></IconButton>
+                      <TableCell align="center">
+                        <IconButton aria-label="findinpage">
+                          <Link to={`${match.url}/${sales.id}`} className={classes.link}>
+                            <FindInPageIcon fontSize="small" />
+                          </Link>                          
+                        </IconButton>
+
+                        <IconButton aria-label="edit">
+                          <Link to={`${match.url}/${sales.id}/edit`} className={classes.link}>
+                            <EditIcon fontSize="small" />                            
+                          </Link>                          
+                        </IconButton>
+
+                        <IconButton aria-label="delete">
+                          <Link to={`${match.url}/${sales.id}/delete`} className={classes.link}>
+                            <DeleteIcon fontSize="small" />
+                          </Link>                          
+                        </IconButton>
                       </TableCell>
 
-                      <TableCell>
-                        <Button tag={Link} to={`${match.url}/${sales.id}`} color="link" size="sm">
-                          {sales.id}
+                      <TableCell align="center">
+                        <Button color="primary">
+                          <Link to={`${match.url}/${sales.id}`} className={classes.link}>{sales.id}</Link>
                         </Button>
                       </TableCell>
 
-                      <TableCell>
+                      <TableCell align="center">
                         {sales.description}
                       </TableCell>
 
-                      <TableCell>
+                      <TableCell align="center">
                         <Translate contentKey={`testApp.State.${sales.state}`} />
                       </TableCell>
 
-                      <TableCell>
+                      <TableCell align="center">
                         {sales.date ? <TextFormat type="date" value={sales.date} format={APP_LOCAL_DATE_FORMAT} /> : null}
                       </TableCell>
                     </TableRow>
